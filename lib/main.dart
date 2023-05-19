2b7d951faaee11e8c817a84f8f1e7b4b0c82b91a
Juan Esteban <estebancode09@gmail.com>
2023-05-19 15:38:44 -0500

    Title: Update App, Description:Creation of a widget in the main to maintain the state of the provider and the get request is created to obtain the data from the api

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:weather/presentation/Screens/home.dart';
 import 'presentation/Screens/cities_list.dart';
+import 'presentation/provider/services/data_services_provider.dart';
 
 void main() => runApp(const MyApp());
 
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (context) => DataServicesProvider(), lazy: false),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/presentation/provider/services/data_services_provider.dart
+++ lib/presentation/provider/services/data_services_provider.dart
@@ -1,21 +1,36 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../infraestructure/models/weather_data.dart';
 
 class DataServicesProvider extends ChangeNotifier {
   final String _urlSite = 'api.openweathermap.org';
-  final String _segmen = '/data/2.5/weather';
+  final String _segment = '/data/2.5/weather';
 
   String? getData;
-  DataServices() {}
+  // ignore: non_constant_identifier_names
+  DataServices() {
+    print('Iniciando servicios');
+    getDataWeather();
+  }
 
   void getDataWeather() async {
-    String city = 'Bello';
+    String city;
+    city = 'Bello';
     final queryparams = {
-      'nameCity': city,
-      'key': '144007da0a2bd09f5c71a6b63ed17545'
+      'q': city,
+      'appid': '144007da0a2bd09f5c71a6b63ed17545'
     };
 
-    final url = Uri.https(_urlSite, _segmen, queryparams);
-
-    //TODO: Obtener data api
+    var url = Uri.https(_urlSite, _segment, queryparams);
+    final response = await http.get(url);
+    final citiesResponse = WeatherData.fromJson(response.body);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData);
+    print(citiesResponse.main);
+    print(getData = citiesResponse.weather[0].description);
+    notifyListeners();
   }
 }

9490311cbc9f7628a6ad3c8ea749f70a001ea30f
Juan Esteban <estebancode09@gmail.com>
2023-05-19 14:02:07 -0500

    Title Update App, Description: Separation of classes and creation of a barrel file

---
+++ lib/shared/exports/exports.dart
@@ -0,0 +1,6 @@
+export 'package:weather/infraestructure/data/coord_data.dart';
+export 'package:weather/infraestructure/data/clouds_data.dart';
+export 'package:weather/infraestructure/data/main_data.dart';
+export 'package:weather/infraestructure/data/sys_data.dart';
+export 'package:weather/infraestructure/data/weather_data.dart';
+export 'package:weather/infraestructure/data/wind_data.dart';
